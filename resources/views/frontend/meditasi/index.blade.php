27c23fd04e16f706fd4f9a0545868a109a6a7d52
faris110900 <faris.riskilail@gmail.com>
2021-01-05 23:16:35 +0700

    meditasi

--- resources/views/frontend/meditasi/index.blade.php
+++ resources/views/frontend/meditasi/index.blade.php
@@ -4,15 +4,71 @@
 
 @section('content')
 
-    <section id="meditasi-instrumen">
+    <section id="meditasi-instrumen" class="meditasi-instrumen">
         <div class="container">
-            <div class="card">
-                <img src="" class="img-fluid">
-                <div class="card-body">
-                    
+            <h3>Suara Kegelapan</h3>
+
+            <div class="row">
+                <div classs="col-lg-4">
+                    <div class="card ml-3">
+                        <img src="{{ asset('assets/img/meditasi/2.jpg') }}" class="img-fluid">
+                        <div class="card-body">
+                            <h5>Nuansa Malam</h5>
+                        </div>
+                    </div>
+                </div>
+                <div classs="col-lg-4">
+                    <div class="card ml-3">
+                        <img src="{{ asset('assets/img/meditasi/3.jpg') }}" class="img-fluid">
+                        <div class="card-body">
+                            <h5>Nuansa Api Unggun</h5>
+                        </div>
+                    </div>
+                </div>
+                <div classs="col-lg-4">
+                    <div class="card ml-3">
+                        <img src="{{ asset('assets/img/meditasi/4.jpg') }}" class="img-fluid">
+                        <div class="card-body">
+                            <h5>Nuansa Angin</h5>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
+
+        <div class="container mt-3">
+            <h3 style="margin-top: 40px;">
+                Suara Binatang
+            </h3>
+            
+            <div class="row">
+                <div classs="col-lg-4">
+                    <div class="card ml-3">
+                        <img src="{{ asset('assets/img/meditasi/2.jpg') }}" class="img-fluid">
+                        <div class="card-body">
+                            <h5>Nuansa Malam</h5>
+                        </div>
+                    </div>
+                </div>
+                <div classs="col-lg-4">
+                    <div class="card ml-3">
+                        <img src="{{ asset('assets/img/meditasi/3.jpg') }}" class="img-fluid">
+                        <div class="card-body">
+                            <h5>Nuansa Api Unggun</h5>
+                        </div>
+                    </div>
+                </div>
+                <div classs="col-lg-4">
+                    <div class="card ml-3">
+                        <img src="{{ asset('assets/img/meditasi/4.jpg') }}" class="img-fluid">
+                        <div class="card-body">
+                            <h5>Nuansa Angin</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </section>
 
 @endsection
